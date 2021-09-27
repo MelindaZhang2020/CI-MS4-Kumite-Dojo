c89f77c0545c3ddf66ec0c75d60d147a0eec652b
Melinda Zhang <melinda_hongye@yahoo.com>
2021-09-27 12:48:55 +0100

    Add home page header for desktop view

--- kumite_dojo/urls.py
+++ kumite_dojo/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 
@@ -27,4 +29,4 @@ urlpatterns = [
         "favicon.png",
         RedirectView.as_view(url=staticfiles_storage.url("img/favicon.png")),
     ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
