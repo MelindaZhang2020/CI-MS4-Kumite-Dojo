f44ac061c91ca4416f9280e54159f65fbe850b5f
Melinda Zhang <melinda_hongye@yahoo.com>
2021-10-07 20:21:04 +0100

    Home: add banner image Feature

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.html import mark_safe
 
-# Create your models here.
+
+class Banner(models.Model):
+    img = models.ImageField(upload_to="banner_imgs/")
+    alt_text = models.CharField(max_length=300)
+
+    def image_tag(self):
+        return mark_safe('<img src="%s" width="100" />' % (self.img.url))
+
+    def __str__(self):
+        return self.alt_text
--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from products.models import Product
+from .models import Banner
 
 
 def index(request):
     """A view to return the index page"""
+    banners = Banner.objects.all().order_by("-id")
     products = Product.objects.filter(is_featured=True).order_by("-id")
-    return render(request, "home/index.html", {"products": products})
+    context = {
+        "products": products,
+        "banners": banners,
+    }
+    return render(request, "home/index.html", context)
