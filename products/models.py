eea1ec651dcefdb70cc024bc550529d0992f02c2
Melinda Zhang <melinda_hongye@yahoo.com>
2021-10-09 20:35:26 +0100

    Home: fix rating star bug

--- products/models.py
+++ products/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     stock = models.IntegerField(default=True)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=False, auto_now=True)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     active = models.BooleanField(default=True)
