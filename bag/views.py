7b11d924a811f73c7cd6b6a6bff57626e1fa2250
Melinda Zhang <melinda_hongye@yahoo.com>
2021-10-12 14:04:59 +0100

    Bag:fetch variation value to bag template

--- bag/views.py
+++ bag/views.py
@@ -16,12 +16,22 @@ def _bag_id(request):
 
 
 def add_to_bag(request, product_id):
-    if request.method == "POST":
-        color = request.POST["color"]
-        size = request.POST["size"]
-        print(color, size)
-
     product = Product.objects.get(id=product_id)  # get the product
+    product_variation = []
+    if request.method == "POST":
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            try:
+                variation = Variation.objects.get(
+                    product=product,
+                    variation_category__iexact=key,
+                    variation_value__iexact=value,
+                )
+                product_variation.append(variation)
+            except:
+                pass
+
     try:
         bag = Bag.objects.get(
             bag_id=_bag_id(request)
@@ -31,24 +41,53 @@ def add_to_bag(request, product_id):
         bag = Bag.objects.create(bag_id=_bag_id(request))
         bag.save()
 
-    try:
-        bag_item = BagItem.objects.get(product=product, bag=bag)  # get the bagitem
+    is_bag_item_exists = BagItem.objects.filter(product=product, bag=bag).exists()
+    if is_bag_item_exists:
+        bag_item = BagItem.objects.filter(product=product, bag=bag)  # get the bagitem
+        # existing variations --> database
+        # current variation   --> product_variation
+        # item_id   --> database
+        ex_var_list = []
+        id = []
+        for item in bag_item:
+            existing_variation = item.variations.all()
+            ex_var_list.append(list(existing_variation))
+            id.append(item.id)
 
-        bag_item.quantity += 1
-        bag_item.save()
+        if product_variation in ex_var_list:
+            # increase bagitem quantity
+            index = ex_var_list.index(product_variation)
+            item_id = id[index]
+            item = BagItem.objects.get(product=product, id=item_id)
+            item.quantity += 1
+            item.save()
+        else:
+            # create new bagitem
+            item = BagItem.objects.create(product=product, quantity=1, bag=bag)
+            if len(product_variation) > 0:
+                item.variations.clear()
+                item.variations.add(*product_variation)
+            item.save()
 
-    except BagItem.DoesNotExist:
+    else:
         bag_item = BagItem.objects.create(
             product=product,
             quantity=1,
             bag=bag,
         )
-
+        if len(product_variation) > 0:
+            bag_item.variations.clear()
+            bag_item.variations.add(*product_variation)
         bag_item.save()
     return redirect("view_bag")
 
 
-def view_bag(request, total=0, quantity=0, bag_items=None):
+def view_bag(
+    request,
+    total=0,
+    quantity=0,
+    bag_items=None,
+):
     """A view that renders the bag contents page and calculate bag cost"""
     try:
         bag = Bag.objects.get(bag_id=_bag_id(request))
@@ -56,16 +95,17 @@ def view_bag(request, total=0, quantity=0, bag_items=None):
         for bag_item in bag_items:
             total += bag_item.product.price * bag_item.quantity
             quantity += bag_item.quantity
-        if total < settings.FREE_DELIVERY_THRESHOLD:
-            delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-            free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-        else:
-            delivery = 0
-            free_delivery_delta = 0
-        grand_total = delivery + total
 
     except ObjectDoesNotExist:
         pass  # just ignore
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+    grand_total = delivery + total
 
     context = {
         "total": total,
@@ -78,21 +118,24 @@ def view_bag(request, total=0, quantity=0, bag_items=None):
     return render(request, "bag/bag.html", context)
 
 
-def remove_from_bag(request, product_id):
+def remove_from_bag(request, product_id, bag_item_id):
     bag = Bag.objects.get(bag_id=_bag_id(request))
     product = get_object_or_404(Product, id=product_id)
-    bag_item = BagItem.objects.get(product=product, bag=bag)
-    if bag_item.quantity > 1:
-        bag_item.quantity -= 1
-        bag_item.save()
-    else:
-        bag_item.delete()
+    try:
+        bag_item = BagItem.objects.get(product=product, bag=bag, id=bag_item_id)
+        if bag_item.quantity > 1:
+            bag_item.quantity -= 1
+            bag_item.save()
+        else:
+            bag_item.delete()
+    except:
+        pass
     return redirect("view_bag")
 
 
-def remove_bag_item(request, product_id):
+def remove_bag_item(request, product_id, bag_item_id):
     bag = Bag.objects.get(bag_id=_bag_id(request))
     product = get_object_or_404(Product, id=product_id)
-    bag_item = BagItem.objects.get(product=product, bag=bag)
+    bag_item = BagItem.objects.get(product=product, bag=bag, id=bag_item_id)
     bag_item.delete()
     return redirect("view_bag")
