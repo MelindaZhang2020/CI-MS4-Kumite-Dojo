e87f7324deebc7c9d04d07309dbaedd85ae4db4f
Melinda Zhang <melinda_hongye@yahoo.com>
2021-10-13 11:42:31 +0100

    Home: add banners tags and fit products in container all products page

--- home/models.py
+++ home/models.py
@@ -4,6 +4,9 @@ from django.utils.html import mark_safe
 
 class Banner(models.Model):
     img = models.ImageField(upload_to="banner_imgs/")
+    label = models.CharField(max_length=500, null=True, blank=True)
+    tag = models.CharField(max_length=500, null=True, blank=True)
+    url = models.URLField(max_length=500, null=True, blank=True)
     alt_text = models.CharField(max_length=300)
 
     def image_tag(self):
--- kumite_dojo/settings.py
+++ kumite_dojo/settings.py
@@ -89,6 +89,7 @@ TEMPLATES = [
                 "django.contrib.messages.context_processors.messages",
                 "django.template.context_processors.media",
                 "bag.context_processors.counter",
+                "bag.context_processors.bag_contents",
             ],
             "builtins": [
                 "crispy_forms.templatetags.crispy_forms_tags",
