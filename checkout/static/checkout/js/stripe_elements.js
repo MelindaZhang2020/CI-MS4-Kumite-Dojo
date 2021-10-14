c2e657423aae97861926002620583fd12075ad21
Melinda Zhang <melinda_hongye@yahoo.com>
2021-10-14 12:55:05 +0100

    Checkout: add basic checkout functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -41,7 +41,7 @@ var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
     ev.preventDefault();
-    card.update({ 'disabled': true});
+    card.update({ 'disabled': true}); //prevent multiple submissons
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
--- checkout/views.py
+++ checkout/views.py
@@ -5,6 +5,8 @@ from .forms import OrderForm
 from bag.views import _bag_id
 from bag.models import Bag
 from django.conf import settings
+from bag.context_processors import bag_contents
+import stripe
 
 
 def checkout(request):
@@ -15,12 +17,30 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse("products"))
 
+    current_bag = bag_contents(request)
+    total = current_bag["grand_total"]
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(
+            request,
+            "Stripe public key is missing. \
+            Did you forget to set it in your environment?",
+        )
+
     template = "checkout/checkout.html"
     context = {
         "order_form": order_form,
         "stripe_public_key": stripe_public_key,
-        "client_secret": "test client secret",
+        "client_secret": intent.client_secret,
     }
 
     return render(request, template, context)
