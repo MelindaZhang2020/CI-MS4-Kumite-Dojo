191990ef72ecde7676c36d8a01a7fd646f29478e
Melinda Zhang <melinda_hongye@yahoo.com>
2021-10-14 12:12:45 +0100

    Checkout: add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -4,9 +4,12 @@ from django.contrib import messages
 from .forms import OrderForm
 from bag.views import _bag_id
 from bag.models import Bag
+from django.conf import settings
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     bag = request.session.session_key
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -16,6 +19,8 @@ def checkout(request):
     template = "checkout/checkout.html"
     context = {
         "order_form": order_form,
+        "stripe_public_key": stripe_public_key,
+        "client_secret": "test client secret",
     }
 
     return render(request, template, context)
--- kumite_dojo/settings.py
+++ kumite_dojo/settings.py
@@ -178,6 +178,14 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+# Stripe
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = "usd"
+STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
+STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET")
+DEFAULT_FROM_EMAIL = "boutiqueado@example.com"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
