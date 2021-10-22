4c3b7f578add7695486fd465db5512dc9a1c9d64
Melinda Zhang <melinda_hongye@yahoo.com>
2021-10-22 15:36:53 +0100

    Deployment: add Procfile ,allowed hosts and set collectstatic =1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kumite_dojo.wsgi:application
--- kumite_dojo/settings.py
+++ kumite_dojo/settings.py
@@ -34,6 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "6467-2a02-8084-81a0-5380-8901-616d-4ea-b731.ngrok.io",
+    "kumite-dojo.herokuapp.com",
 ]
 
 
@@ -120,13 +121,15 @@ WSGI_APPLICATION = "kumite_dojo.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if "DATABASE_URL" in os.environ:
+    DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
 
 
 # Password validation
